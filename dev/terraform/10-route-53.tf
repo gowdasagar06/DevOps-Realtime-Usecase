2a28809a5023a8103b653a1faf5cb05b3d0b17c6
gowdasagar06 <sagargowda6666@gmail.com>
2024-07-23 13:15:21 +0530

    pu

--- dev/terraform/10-route-53.tf
+++ dev/terraform/10-route-53.tf
@@ -1,59 +1,59 @@
-resource "aws_route53_zone" "hosted-zone" {
-  name = "gowdasagar.online"
+# resource "aws_route53_zone" "hosted-zone" {
+#   name = "gowdasagar.online"
 
-  tags = {
-    Name = "test-hosted-zone"
-    provision = "terraform"
-  }
-}
+#   tags = {
+#     Name = "test-hosted-zone"
+#     provision = "terraform"
+#   }
+# }
 
 
-resource "aws_route53_record" "alias-record" {
-  zone_id = aws_route53_zone.hosted-zone.zone_id
-  name    = var.ingress_record
-  type    = "A"
+# resource "aws_route53_record" "alias-record" {
+#   zone_id = aws_route53_zone.hosted-zone.zone_id
+#   name    = var.ingress_record
+#   type    = "A"
 
-  alias {
-    name                   = data.aws_lb.ingress.dns_name
-    zone_id                = data.aws_lb.ingress.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = data.aws_lb.ingress.dns_name
+#     zone_id                = data.aws_lb.ingress.zone_id
+#     evaluate_target_health = true
+#   }
+# }
 
-resource "aws_route53_record" "grafana-record" {
-  zone_id = aws_route53_zone.hosted-zone.zone_id
-  name    = var.grafana_record
-  type    = "A"
+# resource "aws_route53_record" "grafana-record" {
+#   zone_id = aws_route53_zone.hosted-zone.zone_id
+#   name    = var.grafana_record
+#   type    = "A"
 
-  alias {
-    name                   = data.aws_lb.grafana.dns_name
-    zone_id                = data.aws_lb.grafana.zone_id
-    evaluate_target_health = true
-  }
-}
-resource "aws_route53_record" "prometheus-record" {
-  zone_id = aws_route53_zone.hosted-zone.zone_id
-  name    = var.prometheus_record
-  type    = "A"
+#   alias {
+#     name                   = data.aws_lb.grafana.dns_name
+#     zone_id                = data.aws_lb.grafana.zone_id
+#     evaluate_target_health = true
+#   }
+# }
+# resource "aws_route53_record" "prometheus-record" {
+#   zone_id = aws_route53_zone.hosted-zone.zone_id
+#   name    = var.prometheus_record
+#   type    = "A"
 
-  alias {
-    name                   = data.aws_lb.prometheus.dns_name
-    zone_id                = data.aws_lb.prometheus.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = data.aws_lb.prometheus.dns_name
+#     zone_id                = data.aws_lb.prometheus.zone_id
+#     evaluate_target_health = true
+#   }
+# }
 
-data "aws_lb" "ingress" {
-  arn  = var.ingress_arn
-  name = var.ingress_name
-}
+# data "aws_lb" "ingress" {
+#   arn  = var.ingress_arn
+#   name = var.ingress_name
+# }
 
-data "aws_lb" "grafana" {
-  arn  = var.grafana_arn
-  name = var.grafana_name
-}
+# data "aws_lb" "grafana" {
+#   arn  = var.grafana_arn
+#   name = var.grafana_name
+# }
 
-data "aws_lb" "prometheus" {
-  arn  = var.prometheus_arn
-  name = var.prometheus_name
-}
+# data "aws_lb" "prometheus" {
+#   arn  = var.prometheus_arn
+#   name = var.prometheus_name
+# }
